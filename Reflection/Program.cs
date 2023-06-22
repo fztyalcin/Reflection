da171ec0feb046d23434446709fff213ab0f4473
Yalçın ORUÇ <Yaln ORU@DESKTOP-7HODA89>
2023-06-23 00:38:08 +0300

    Add project files.

---
+++ Reflection/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(ReflectionOrnek);
+
+
+            // Özelliklerin Bilgilerini alıyoruz
+
+            PropertyInfo[] properties = type.GetProperties(); 
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine("Property: " + property.Name);
+            }
+
+            // Metodların Bilgilerini Alıyoruz
+
+            MethodInfo[] methods = type.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine("Method: " +  method.Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Reflection/ReflectionOrnek.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Reflection
+{
+
+    //"Reflection" (yansıma) terimi, bir programın çalışma zamanında kendi yapısını inceleme ve değiştirme yeteneğini ifade eder.
+    //Yani, bir nesnenin veya türünün (type) bilgilerine dinamik olarak erişme ve bunları manipüle etme işlemidir.
+    //Reflection, C# dilinde geliştiricilere dinamik programlama, metaprogramlama ve veri serileştirme gibi çeşitli senaryolar için güçlü bir araç sağlar.
+
+    public class ReflectionOrnek
+    {
+        public int Property1 { get; set; }
+        public string Property2 { get; set; }
+
+        public void Method1 () 
+        {
+            Console.WriteLine("Method 1 Called");
+        }
+
+        public void Method2 (string parameter)
+        {
+            Console.WriteLine ("Method 2 Called" +  parameter);
+        }
+
+    }
+}
